a8627d43d9b21c112c5fa17a3e934128258abe99
kevinlekiller <kevinlekiller@gmail.com>
2013-09-15 11:38:24 -0400

    Check if all the URL parameters are sent.

--- php/frontend/getnzb.php
+++ php/frontend/getnzb.php
@@ -1,16 +1,20 @@
 <?php
-require_once('config.php');
-require_once(PHP_DIR.'backend/nzb.php');
-$nzb = new nzb;
-$subject = substr($_GET['subject'], 0, 150);
-$nzbfile = $nzb->createNZB($_GET['type'], $_GET['identifier'], $_GET['group'], $subject);
-if ($nzbfile === false)
-	print "Not Found, data doesn't exist?.";
-else
+if (isset($_GET['type']) && isset($_GET['subject']) && isset($_GET['identifier']) && isset($_GET['group']))
 {
-	header('Content-type: application/x-nzb');
-	header('Content-Disposition: attachment; filename="'.preg_replace('/\.$|[^\w.\[\]()-]/', '', $subject).'.nzb"');
-	print $nzbfile;
+	require_once('config.php');
+	require_once(PHP_DIR.'backend/nzb.php');
+	$nzb = new nzb;
+	$subject = substr($_GET['subject'], 0, 150);
+	$nzbfile = $nzb->createNZB($_GET['type'], $_GET['identifier'], $_GET['group'], $subject);
+	if ($nzbfile === false)
+		print "Not Found, data doesn't exist?.";
+	else
+	{
+		header('Content-type: application/x-nzb');
+		header('Content-Disposition: attachment; filename="'.preg_replace('/\.$|[^\w.\[\]()-]/', '', $subject).'.nzb"');
+		print $nzbfile;
+	}
 }
-
+else
+	print 'Invalid request, make sure you sent all the variables.';
 ?>

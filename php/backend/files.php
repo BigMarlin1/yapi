9e340d1eb1a115383de7205459822750ea79e912
kevinlekiller <kevinlekiller@gmail.com>
2013-09-17 14:03:49 -0400

    Update search query like browse query.

--- php/backend/files.php
+++ php/backend/files.php
@@ -178,10 +178,45 @@ Class files
 			foreach ($tids as $tid)
 			{
 				$id = $tid['id'];
+				$startq = $db->query(sprintf('SELECT DISTINCT(chash) AS c FROM files_%d WHERE 1=1 %s %s ORDER BY utime DESC LIMIT %d OFFSET %d', $id, $subq, $age, $max, $offset));
+				$scount = count($startq);
+
 				if ($i++ == $count)
-					$fstr .= sprintf('(SELECT *, SUM(fsize) AS size, SUM(parts) AS totalparts, SUM(partsa) AS actualparts, MAX(nstatus) AS nstatust FROM files_%d WHERE 1=1 %s %s GROUP BY chash ORDER BY utime DESC LIMIT %d OFFSET %d)', $id, $subq, $age, $max, $offset);
+				{
+					if ($scount == 0)
+						$fstr = '(SELECT 1=1)';
+					else
+					{
+						$fstr .= '(SELECT *, SUM(fsize) AS size, SUM(parts) AS totalparts, SUM(partsa) AS actualparts, MAX(nstatus) AS nstatust FROM files_'.$id.' WHERE chash IN (';
+						$now = 1;
+						foreach ($startq as $c)
+						{
+							if ($now++ < $scount)
+								$fstr .= "'".$c['c']."',";
+							else
+								$fstr .= "'".$c['c']."'";
+						}
+						$fstr .= ') GROUP BY chash ORDER BY utime DESC LIMIT '.$max.')';
+					}
+				}
 				else
-					$fstr .= sprintf('(SELECT *, SUM(fsize) AS size, SUM(parts) AS totalparts, SUM(partsa) AS actualparts, MAX(nstatus) AS nstatust FROM files_%d WHERE 1=1 %s %s GROUP BY chash ORDER BY utime DESC LIMIT %d OFFSET %d) UNION ', $id, $subq, $age, $max, $offset);
+				{
+					if ($scount == 0)
+						continue;
+					else
+					{
+						$fstr .= '(SELECT *, SUM(fsize) AS size, SUM(parts) AS totalparts, SUM(partsa) AS actualparts, MAX(nstatus) AS nstatust FROM files_'.$id.' WHERE chash IN (';
+						$now = 1;
+						foreach ($startq as $c)
+						{
+							if ($now++ < $scount)
+								$fstr .= "'".$c['c']."',";
+							else
+								$fstr .= "'".$c['c']."'";
+						}
+						$fstr .= ') GROUP BY chash ORDER BY utime DESC LIMIT '.$max.') UNION';
+					}
+				}
 			}
 			return $db->query('SELECT files.*, groups.name, groups.id AS groupid FROM ('.$fstr.') AS files INNER JOIN groups ON groups.id = files.groupid ORDER BY utime DESC LIMIT '.MAX_PERPAGE, true);
 		}
